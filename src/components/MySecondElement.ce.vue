f857492bec77e463c55c1f11d476b07cacd23555
mars073 <imars073@live.be>
2022-05-02 23:25:31 +0200

    Add prop

--- src/components/MySecondElement.ce.vue
+++ src/components/MySecondElement.ce.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="custom-css">
-    <h1>Hi Mom !</h1>
+    <h1>Hi {{ name }} !</h1>
     <h2>I'm on github !</h2>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'World'
+    }
+  }
+}
+</script>
 
 <style>
 .custom-css {
