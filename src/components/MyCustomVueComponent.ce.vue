50210db86d7c562b8c4f05c303d7dc2ee41e0fce
mars073 <imars073@live.be>
2022-05-02 23:13:07 +0200

    add some css

--- src/components/MyCustomVueComponent.ce.vue
+++ src/components/MyCustomVueComponent.ce.vue
@@ -1,7 +1,14 @@
 <template>
   <header>
-    <p>neted :</p>
+    <p>nested :</p>
     <my-second-element />
     <hr />
   </header>
 </template>
+
+<style>
+header {
+  background: lightgray;
+  padding-left: 10px;
+}
+</style>
